7377e91468b790eec80846d709af4282b4945f47
Vaibhav Mali <malivp3494@gmail.com>
2020-05-03 09:57:47 +0530

    script to mount drives

---
+++ mount-drives.sh
@@ -0,0 +1,9 @@
+sudo mkdir -p /mnt/one
+sudo mkdir -p /mnt/two
+sudo mkdir -p /mnt/three
+sudo mkdir -p /mnt/four
+
+sudo mount /dev/sda4 /mnt/one
+sudo mount /dev/sda6 /mnt/two
+sudo mount /dev/sda7 /mnt/three
+sudo mount /dev/sda8 /mnt/four
