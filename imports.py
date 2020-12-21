b8fc14a0a9ad09a883736e946424b4c7703e7ed7
toxicdesire <malivp3494@gmail.com>
2020-12-21 14:35:53 +0530

    Fixing imports for convenience

--- imports.py
+++ imports.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+
+# this is just for using shell as a handy calculator
+# it's aliased as pc, with command: python3 -i imports.py
 from math import *
 import os
 import sys
 import shlex
 import subprocess
+
+# combinations
+def C(n,r):
+    return factorial(n)/(factorial(r)*factorial(n-r))
+
+# permutations
+def P(n,r):
+    return factorial(n)/(factorial(n-r))
--- nconvert.py
+++ nconvert.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
+import sys
 # number systems
-base = int(input("Base : "))
-n = int(input("Number : "), base)
-print(f'Decimal:{n}')
-print(f'Binary:{bin(n)}')
-print(f'Octal:{oct(n)}')
-print(f'Hex:{hex(n)}')
+args = sys.argv
+nums = []
+def print_all(nums, base):
+    for n in nums:
+        print(f'Decimal:{n}')
+        print(f'Binary:{bin(n)}')
+        print(f'Octal:{oct(n)}')
+        print(f'Hex:{hex(n)}')
+
+if len(args) <=1:
+    base = int(input("Base : "))
+    n = int(input("Number : "), base)
+    nums.append(n)
+elif len(args) == 2:
+    base = 10
+    n = int(args[1])
+    nums.append(int(n, base))
+else:
+    base = int(args[1])
+    nums = [int(i,base) for i in args[2:]]
+
+print_all(nums, base)
